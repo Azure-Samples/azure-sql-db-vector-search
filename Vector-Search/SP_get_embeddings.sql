cd5e50f7cdfd29fe21793702447feed641313d0c
Pookam90 <60406274+Pookam90@users.noreply.github.com>
2024-05-08 20:16:08 +0530

    Update SP_get_embeddings.sql

--- Vector-Search/SP_get_embeddings.sql
+++ Vector-Search/SP_get_embeddings.sql
@@ -3,58 +3,32 @@ Using a call to a REST service to get embeddings is just one of the integration 
 You can let any of the available models access data stored in Azure SQL Database to create solutions where your users can interact with the data as shown here 
 */
 
-CREATE PROCEDURE [dbo].[get_embeddings]
+create procedure [dbo].[get_embeddings]
 (
-    @model VARCHAR(MAX),
-    @text NVARCHAR(MAX),
-    @embedding VARBINARY(8000) OUTPUT
+    @model varchar(max),
+    @text nvarchar(max),
+    @embedding varbinary(8000) output
 )
-AS
-BEGIN
-    DECLARE @retval INT, @response NVARCHAR(MAX);
-    DECLARE @url VARCHAR(MAX);
-    DECLARE @payload NVARCHAR(MAX) = json_object('input': @text);
+as
+begin
+    declare @retval int, @response nvarchar(max);
+    declare @url varchar(max);
+    declare @payload nvarchar(max) = json_object('input': @text);
 
     -- Set the @url variable with proper concatenation before the exec statement
-    SET @url = 'https://<yourdeploymentname>.openai.azure.com/openai/deployments/' + @model + '/embeddings?api-version=2023-03-15-preview';
+    set @url = 'https://<resourcename>.openai.azure.com/openai/deployments/' + @model + '/embeddings?api-version=2023-03-15-preview';
 
-    EXEC dbo.sp_invoke_external_rest_endpoint 
+    exec dbo.sp_invoke_external_rest_endpoint 
         @url = @url,
         @method = 'POST',   
         @payload = @payload,   
-        @headers = '{"Content-Type":"application/json", "api-key":"<APIKEY>"}', 
-        @response = @response OUTPUT;
+        @headers = '{"Content-Type":"application/json", "api-key":"<openAIkey>"}', 
+        @response = @response output;
 
-    DECLARE @jsonArray NVARCHAR(MAX) = '';
-    DECLARE @first BIT = 1;
+    -- Use json_query to extract the embedding array directly
+    declare @jsonArray nvarchar(max) = json_query(@response, '$.result.data[0].embedding');
 
-    -- Use a cursor to iterate through the JSON elements and build the array
-    DECLARE @value NVARCHAR(MAX);
-    DECLARE json_cursor CURSOR FOR
-    SELECT [value]
-    FROM OPENJSON(@response, '$.result.data[0].embedding');
-
-    OPEN json_cursor;
-    FETCH NEXT FROM json_cursor INTO @value;
-
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-        IF @first = 1
-            SET @first = 0;
-        ELSE
-            SET @jsonArray = @jsonArray + ',';
-
-        SET @jsonArray = @jsonArray + CAST(@value AS NVARCHAR(MAX));
-        FETCH NEXT FROM json_cursor INTO @value;
-    END;
-
-    CLOSE json_cursor;
-    DEALLOCATE json_cursor;
-
-    -- Enclose the values in brackets to form a valid JSON array
-    SET @jsonArray = '[' + @jsonArray + ']';
-
-    -- Convert the JSON array to a vector
-    SET @embedding = json_array_to_vector(@jsonArray);
-END
+    -- Assuming json_array_to_vector can handle the JSON array format
+    set @embedding = json_array_to_vector(@jsonArray);
+end
 GO
