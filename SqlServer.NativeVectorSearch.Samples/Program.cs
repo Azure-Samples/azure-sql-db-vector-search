4bdaae3cc333acf2e72c3ed7eb6709e845a1f2a0
Damir Dobric <ddobric@daenet.com>
2024-11-12 23:11:43 +0100

    initial commit

---
+++ SqlServer.NativeVectorSearch.Samples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SqlServer.NativeVectorSearch.Samples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
